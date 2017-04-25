176a29baf15afdb93c3b34b3a41d2ce4487c99e3
ReedS <ReedStock1992@gmail.com>
2017-04-25 11:06:15 -0500

    Adding manager functionality

---
+++ src/Manager/Checkin.java
@@ -0,0 +1,22 @@
+package Manager;
+
+/**
+ * Created by ReedS on 4/24/2017.
+ */
+public class Checkin {
+
+    // Variables
+    public String customerID;
+    public String flightID;
+
+    // Constructor
+    Checkin(String customerID, String flightID){
+        this.customerID = customerID;
+        this.flightID = flightID;
+    }
+
+    // Update checkin database
+    public void checkedIn(){
+
+    }
+}
