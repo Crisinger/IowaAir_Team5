1000f843c0d0dffe3057a14197ea7dc1e0aec92d
crisinger <crisinger13@gmail.com>
2017-05-01 13:42:49 -0500

    addBag implementation

--- src/Manager/ManagerCheckIn.java
+++ src/Manager/ManagerCheckIn.java
@@ -1,5 +1,7 @@
 package Manager;
 
+import General.AccountFunctions;
+
 import java.sql.Connection;
 import java.sql.Statement;
 
@@ -11,7 +13,6 @@ public class ManagerCheckIn {
     public static void managerCheckIn(Connection con, int referenceID){
         Statement stmt = null;
         try {
-            con.setAutoCommit(false);
             stmt = con.createStatement();
             stmt.executeUpdate("UPDATE BOOKEDFLIGHTS SET checkedIn = 1 WHERE ID = " + referenceID +";");
             stmt.close();
@@ -24,7 +25,6 @@ public class ManagerCheckIn {
     public static void managerCancelFlight(Connection con, int referenceID){
         Statement stmt = null;
         try {
-            con.setAutoCommit(false);
             stmt = con.createStatement();
             stmt.executeUpdate("DELETE FROM BOOKEDFLIGHTS WHERE ID = " + referenceID +";");
             stmt.close();
@@ -34,18 +34,19 @@ public class ManagerCheckIn {
         }
     }
 
-   /*public static void managerBookFlight(Connection con, int referenceID){
+   public static void addBag(Connection con, int referenceID){
         Statement stmt = null;
         try {
-            con.setAutoCommit(false);
             stmt = con.createStatement();
-            stmt.executeUpdate("DELETE FROM BOOKEDFLIGHTS WHERE ID = " + referenceID +";");
+            stmt.executeUpdate("UPDATE BOOKEDFLIGHTS SET bags = bags + 1 WHERE ID = " + referenceID +";");
             stmt.close();
         } catch (Exception e){
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
-    } */
+    }
+
+
 
 
 }
